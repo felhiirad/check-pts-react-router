97a017c98eeb2df48cf70a75e8d5bc95f51c7ed8
marwenabbassy <marwen.abbassy2021@gmail.com>
2020-09-07 00:22:28 +0200

    react router check point

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Route, BrowserRouter, Link, Switch, Router } from 'react-router-dom';
+import Category from './components/Category/Category';
+import Products from './components/Products/Products';
+import AdminArea from './components/AdminArea/AdminArea';
+import Home from './components/Home/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      < BrowserRouter>
+
+      <Switch>
+        <nav>
+          <ul>
+            <li><Link to="/home">Home</Link></li>
+            <li><Link to="/category">Category</Link></li>
+            <li><Link to="/products">Products</Link></li>
+            <li><Link to="/admin-area">Admin Area</Link></li>
+          </ul>
+        </nav>
+      </Switch>
+      </ BrowserRouter>
+      < BrowserRouter>
+      <Switch>
+      <Route path="/home" exact component={Home}/>
+      <Route path="/category" component={Category}/>
+      <Route path="/products" component={Products}/>
+      <Route path="/admin-area" component={AdminArea}/>
+      </Switch>
+      </ BrowserRouter>
     </div>
   );
 }
